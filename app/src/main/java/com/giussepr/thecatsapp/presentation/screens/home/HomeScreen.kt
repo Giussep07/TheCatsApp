37eed6a67136eef35bc8c2186bb8ebd315bddfa4
giussepr <giussepr@gmail.com>
2023-04-13 17:15:10 -0500

    Add coil to version catalog, adjust Home Screen to match with designs

--- app/src/main/java/com/giussepr/thecatsapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/giussepr/thecatsapp/presentation/screens/home/HomeScreen.kt
@@ -1,19 +1,36 @@
 package com.giussepr.thecatsapp.presentation.screens.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.giussepr.thecatsapp.domain.model.Cat
 
 @Preview
 @Composable
@@ -24,19 +41,119 @@ fun HomeScreenPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController, viewModel: HomeViewModel = hiltViewModel()) {
+    LaunchedEffect(key1 = true) {
+        viewModel.onUiEvent(HomeViewModel.HomeUiEvent.LoadCats)
+    }
+
     Scaffold(
         modifier = Modifier.background(color = MaterialTheme.colorScheme.background),
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "The Cats App")
+                    Text(
+                        text = "Catbreeds",
+                        textAlign = TextAlign.Center,
+                    )
                 })
         }
     ) { paddingValues ->
         Column(
             modifier = Modifier.padding(paddingValues)
         ) {
-            Text(text = "Home Screen")
+
+            if (viewModel.uiState.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            if (viewModel.uiState.cats.isNotEmpty()) {
+                HomeScreenContent(
+                    cats = viewModel.uiState.cats,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreenContent(
+    cats: List<Cat>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(cats) { cat ->
+            CatCardItem(
+                cat = cat,
+                modifier = Modifier.testTag(cat.id)
+            )
+        }
+    }
+}
+
+@Composable
+fun CatCardItem(
+    cat: Cat,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // Cat breed
+            Text(
+                text = cat.breedName,
+                style = MaterialTheme.typography.titleMedium
+            )
+            // Cat Image
+            AsyncImage(
+                modifier = Modifier.padding(top = 16.dp),
+                model = cat.imageUrl,
+                contentDescription = ""
+            )
+            // Origin and Intelligence
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                // Origin
+                Text(
+                    text = "Origin: ${cat.origin}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                // Intelligence
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = "Intelligence: ",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    for (i in 1..cat.intelligence) {
+                        Text(
+                            text = "⭐",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/giussepr/thecatsapp/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/giussepr/thecatsapp/presentation/screens/home/HomeViewModel.kt
@@ -1,7 +1,11 @@
 package com.giussepr.thecatsapp.presentation.screens.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.giussepr.thecatsapp.domain.model.Cat
 import com.giussepr.thecatsapp.domain.model.fold
 import com.giussepr.thecatsapp.domain.repository.CatsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,16 +21,43 @@ class HomeViewModel @Inject constructor(
     private val catsRepository: CatsRepository
 ): ViewModel() {
 
-    fun getCats() {
+    var uiState by mutableStateOf(HomeUiState())
+        private set
+
+    private fun getCats() {
         catsRepository.getCats().map { result ->
             result.fold(
                 onSuccess = { cats ->
-                    // TODO: Show cats
+                    uiState = uiState.copy(
+                        isLoading = false,
+                        cats = cats
+                    )
                 },
                 onFailure = {
-                    // TODO: Show error
+                    uiState = uiState.copy(
+                        isLoading = false,
+                        errorMessage = it.message
+                    )
                 }
             )
-        }.flowOn(Dispatchers.IO).launchIn(viewModelScope)
+        }
+            .onStart { uiState = uiState.copy(isLoading = true) }
+            .flowOn(Dispatchers.IO).launchIn(viewModelScope)
+    }
+
+    fun onUiEvent(uiEvent: HomeUiEvent) {
+        when (uiEvent) {
+            is HomeUiEvent.LoadCats -> { getCats() }
+        }
+    }
+
+    data class HomeUiState(
+        val isLoading: Boolean = false,
+        val errorMessage: String = "",
+        val cats: List<Cat> = emptyList()
+    )
+
+    sealed class HomeUiEvent {
+        object LoadCats : HomeUiEvent()
     }
 }
